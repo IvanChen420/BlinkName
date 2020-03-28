5dfc7531c7332e71e9f3940bbec8167ee820f3c6
IvanChen420 <62791082+IvanChen420@users.noreply.github.com>
2020-03-28 17:17:55 +0800

    Add files via upload

---
+++ blinkname.ino
@@ -0,0 +1,107 @@
+
+
+//define D7 a varible name to avoid writing D7 repidly
+int led1 = D7;
+
+void setup() {
+
+    pinMode = (led1,OUTPUT);//setup led 1 set output
+
+}
+
+void loop() {
+    //i
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);
+    digitalWrite(led1,LOW);
+    delay(1500);
+    //v
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(1500);//1.5 sec
+    digitalWirte(led1,LOW);
+    delay(1500);
+    //a
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(500);
+    delay(1500);//1.5 sec
+    digitalWirte(led1,LOW);
+    delay(1500);
+    //n
+    delay(1500);//1.5 sec
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(3500);
+    
+    //C
+	digitalWrite(led1, HIGH);
+    delay(1500);//long blink
+    digitalWrite(led1, LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(500);
+	digitalWrite(led1, HIGH);
+    delay(1500);//long blink
+    digitalWrite(led1, LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);
+    digitalWrite(led1,LOW);
+    delay(1500);
+    //H
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);//1.5 sec
+    digitalWirte(led1,LOW);
+    delay(1500);
+    //E
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(1500);
+    //n
+    delay(1500);//1.5 sec
+    digitalWirte(led1,LOW);
+    delay(500);
+    digitalWrite(led1,HIGH);
+    delay(500);//half sec
+    digitalWirte(led1,LOW);
+    delay(1500);
+
+    
+    
+
+}
